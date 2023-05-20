12212f7e7f6ca60a765e6dc3d5c43aad58f06c34
Q_Zhou <QZhou@gmail.com>
2023-05-20 14:05:11 +0800

    load Fonts nunito

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 import HomeStack from './routes/homestack';
 import SettingsDrawer from './routes/drawer';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+  });
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <SettingsDrawer />
     </NavigationContainer>
   );
+
 }
