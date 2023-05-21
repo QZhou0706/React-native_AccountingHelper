9e1c39aa7c3390ebabae852109c08e45416f93b8
Q_Zhou <QZhou@gmail.com>
2023-05-21 18:48:30 +0800

    add bottomTab shadow

--- routes/tab.js
+++ routes/tab.js
@@ -21,21 +21,29 @@ const TabList = [
 
 const Tab = createBottomTabNavigator();
 
-const animate1 = { 0: { scale: 1}, 1: { scale: 1.3} };
-const animate2 = { 0: { scale: 1.3}, 1: { scale: 1} };
+const animate1 = { 0: { scale: 1, translateY: 0 }, 1: { scale: 1.4, translateY: -20 } };
+const animate2 = { 0: { scale: 1.4, translateY: -20 }, 1: { scale: 1, translateY: 0 } };
 
+const circle1 = { 0: {scale: 0}, 0.3: {scale: .5}, 0.5: {scale: .3}, 0.8: {scale: .7}, 1: {scale: 1}};
+const circle2 = { 0: {scale: 1}, 1: {scale: 0}};
 
 const TabButton = (props) => {
   const { item, onPress, accessibilityState } = props;
   const focused = accessibilityState.selected;
   const viewRef = useRef(null);
+  const circleRef = useRef(null);
+  const textRef = useRef(null);
 
   useEffect(() => {
     if (focused) {
       viewRef.current.animate(animate1);
+      circleRef.current.animate(circle1);
+      textRef.current.transitionTo({scale: 1});
     }
     else {
       viewRef.current.animate(animate2);
+      circleRef.current.animate(circle2);
+      textRef.current.transitionTo({scale: 0});
     }
   }, [focused])
 
@@ -46,11 +54,20 @@ const TabButton = (props) => {
       style={styles.container}>
       <Animatable.View
         ref={viewRef}
-        duration={1000}
+        duration={400}
         style={styles.container}>
         <View style={styles.tabButton}>
-          <item.iconType name={item.activeIcon} color={'#f76666'} size={24} />
+          <Animatable.View 
+          ref={circleRef}
+          duration={400}
+          style={{...StyleSheet.absoluteFillObject, backgroundColor: '#f76666', borderRadius: 25,}} />
+          <item.iconType name={focused ? item.activeIcon : item.inActiveIcon} color={focused? '#fff': '#f76666'} size={20} />
         </View>
+        <Animatable.Text 
+        ref={textRef}
+        style={{fontSize: 10, color: '#333', fontFamily: 'nunito-bold', opacity: 0.7}}>
+          {item.label}
+        </Animatable.Text>
       </Animatable.View>
     </TouchableOpacity>
   )
@@ -63,7 +80,7 @@ const TabNavigator = () => {
         headerShown: false,
         tabBarShowLabel: false,
         tabBarStyle: {
-          height: 70,
+          height: 60,
           marginBottom: 20,
           position: 'absolute',
           borderRadius: 16,
@@ -96,7 +113,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    top: 8,
+    top: 5,
+    elevation: 1,
+    shadowColor: '#ccc',
+    shadowOpacity: 0.2,
+    shadowOffset: {width: 0, height: 1},
   },
   tabButton: {
     width: 40,
@@ -104,5 +125,10 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#fff',
+    borderColor: '#fff',
+    borderRadius: 25,
+    borderWidth: 5,
+    elevation: 1,
   }
 })
