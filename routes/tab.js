c8aefc416b90331d4e59c0bc60da9fb7f5cab950
Q_Zhou <QZhou@gmail.com>
2023-05-21 17:55:07 +0800

    modify tab

--- routes/tab.js
+++ routes/tab.js
@@ -1,6 +1,6 @@
 import * as Animatable from 'react-native-animatable';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
 import { useEffect, useRef } from 'react';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -9,6 +9,7 @@ import AntIcon from "react-native-vector-icons/AntDesign";
 import DrawerNavigator from '../routes/drawer';
 import AboutStack from './aboutStack';
 import AddItem from '../screens/addItem';
+import { Transition } from 'react-native-reanimated';
 
 
 
@@ -20,29 +21,21 @@ const TabList = [
 
 const Tab = createBottomTabNavigator();
 
-const animate1 = { 0: { scale: 1, translateY: 20 }, 1: { scale: 1.2, translateY: -15 } };
-const animate2 = { 0: { scale: 1.2, translateY: -15 }, 1: { scale: 1, translateY: 20 } };
+const animate1 = { 0: { scale: 1}, 1: { scale: 1.3} };
+const animate2 = { 0: { scale: 1.3}, 1: { scale: 1} };
 
-const circle1 = { 0: { scale: 0 }, 1: { scale: 1 } };
-const circle2 = { 0: { scale: 1 }, 1: { scale: 0 } };
 
 const TabButton = (props) => {
   const { item, onPress, accessibilityState } = props;
   const focused = accessibilityState.selected;
   const viewRef = useRef(null);
-  const circleRef = useRef(null);
-  const textRef = useRef(null);
 
   useEffect(() => {
     if (focused) {
       viewRef.current.animate(animate1);
-      circleRef.current.animate(circle1);
-      textRef.current.transitionTo({scale: 1});
     }
     else {
       viewRef.current.animate(animate2);
-      circleRef.current.animate(circle2);
-      textRef.current.transitionTo({scale: 0});
     }
   }, [focused])
 
@@ -56,17 +49,8 @@ const TabButton = (props) => {
         duration={1000}
         style={styles.container}>
         <View style={styles.tabButton}>
-          <Animatable.View
-            ref={circleRef}
-            style={{ ...StyleSheet.absoluteFillObject, backgroundColor: '#f76666', borderRadius: 25 }} />
-          <item.iconType name={item.activeIcon} color={focused ? '#fff' : '#f76666'} size={24} />
+          <item.iconType name={item.activeIcon} color={'#f76666'} size={24} />
         </View>
-        <Animatable.Text
-          ref={textRef}
-          style={{fontSize: 10, textAlign: 'center', }}
-        >
-          {item.label}
-        </Animatable.Text>
       </Animatable.View>
     </TouchableOpacity>
   )
@@ -85,6 +69,8 @@ const TabNavigator = () => {
           borderRadius: 16,
           left: 16,
           right: 16,
+          alignItems: 'center',
+          justifyContent: 'center',
         },
       }
     }}>
@@ -110,15 +96,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    top: 8,
   },
   tabButton: {
     width: 40,
     height: 40,
     borderRadius: 50,
-    borderWidth: 4,
-    borderColor: '#fff',
-    backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
   }
