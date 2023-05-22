16db32fe1485249e455c74ee4a4381b77fc43536
Q_Zhou <QZhou@gmail.com>
2023-05-22 08:55:23 +0800

    add kayboard listening in tabNavigator

--- routes/tab.js
+++ routes/tab.js
@@ -1,6 +1,7 @@
 import * as Animatable from 'react-native-animatable';
+import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Keyboard } from 'react-native';
 import { useEffect, useRef } from 'react';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -24,8 +25,8 @@ const Tab = createBottomTabNavigator();
 const animate1 = { 0: { scale: 1, translateY: 0 }, 1: { scale: 1.4, translateY: -20 } };
 const animate2 = { 0: { scale: 1.4, translateY: -20 }, 1: { scale: 1, translateY: 0 } };
 
-const circle1 = { 0: {scale: 0}, 0.3: {scale: .5}, 0.5: {scale: .3}, 0.8: {scale: .7}, 1: {scale: 1}};
-const circle2 = { 0: {scale: 1}, 1: {scale: 0}};
+const circle1 = { 0: { scale: 0 }, 0.3: { scale: .5 }, 0.5: { scale: .3 }, 0.8: { scale: .7 }, 1: { scale: 1 } };
+const circle2 = { 0: { scale: 1 }, 1: { scale: 0 } };
 
 const TabButton = (props) => {
   const { item, onPress, accessibilityState } = props;
@@ -38,12 +39,12 @@ const TabButton = (props) => {
     if (focused) {
       viewRef.current.animate(animate1);
       circleRef.current.animate(circle1);
-      textRef.current.transitionTo({scale: 1});
+      textRef.current.transitionTo({ scale: 1 });
     }
     else {
       viewRef.current.animate(animate2);
       circleRef.current.animate(circle2);
-      textRef.current.transitionTo({scale: 0});
+      textRef.current.transitionTo({ scale: 0 });
     }
   }, [focused])
 
@@ -57,15 +58,15 @@ const TabButton = (props) => {
         duration={400}
         style={styles.container}>
         <View style={styles.tabButton}>
-          <Animatable.View 
-          ref={circleRef}
-          duration={400}
-          style={{...StyleSheet.absoluteFillObject, backgroundColor: '#f76666', borderRadius: 25,}} />
-          <item.iconType name={focused ? item.activeIcon : item.inActiveIcon} color={focused? '#fff': '#f76666'} size={20} />
+          <Animatable.View
+            ref={circleRef}
+            duration={400}
+            style={{ ...StyleSheet.absoluteFillObject, backgroundColor: '#f76666', borderRadius: 25, }} />
+          <item.iconType name={focused ? item.activeIcon : item.inActiveIcon} color={focused ? '#fff' : '#f76666'} size={20} />
         </View>
-        <Animatable.Text 
-        ref={textRef}
-        style={{fontSize: 10, color: '#333', fontFamily: 'nunito-bold', opacity: 0.7}}>
+        <Animatable.Text
+          ref={textRef}
+          style={{ fontSize: 10, color: '#333', fontFamily: 'nunito-bold', opacity: 0.7 }}>
           {item.label}
         </Animatable.Text>
       </Animatable.View>
@@ -74,27 +75,44 @@ const TabButton = (props) => {
 }
 
 const TabNavigator = () => {
+  
+  // listening keyboard to avoid tabBar moving with kayboard
+  const [keyboardShown, setKeyboardShown] = React.useState(false);
+  React.useEffect(() => {
+    const showSubscription = Keyboard.addListener('keyboardDidShow', () => {
+      setKeyboardShown(true);
+    });
+    const hideSubscription = Keyboard.addListener('keyboardDidHide', () => {
+      setKeyboardShown(false);
+    });
+
+    return () => {
+      showSubscription.remove();
+      hideSubscription.remove();
+    };
+  }, []);
+
   return (
-    <Tab.Navigator screenOptions={() => {
-      return {
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarStyle: {
-          height: 60,
-          marginBottom: 20,
-          position: 'absolute',
-          borderRadius: 16,
-          left: 16,
-          right: 16,
-          alignItems: 'center',
-          justifyContent: 'center',
-        },
-      }
-    }}>
+    <Tab.Navigator
+      screenOptions={() => {
+        return {
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            height: 60,
+            bottom: keyboardShown ? -100 : 20,
+            position: 'absolute',
+            borderRadius: 16,
+            left: 16,
+            right: 16,
+            alignItems: 'center',
+            justifyContent: 'center',
+          },
+        }
+      }}>
       {TabList.map((item, index) => {
         return (
           <Tab.Screen name={item.route} component={item.component} options={{
-            tabBarLabel: item.label,
             tabBarButton: (props) => (
               <TabButton {...props} item={item} />
             ),
@@ -114,10 +132,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     top: 5,
-    elevation: 1,
-    shadowColor: '#ccc',
-    shadowOpacity: 0.2,
-    shadowOffset: {width: 0, height: 1},
+
   },
   tabButton: {
     width: 40,
